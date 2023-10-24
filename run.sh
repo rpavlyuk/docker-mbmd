71754beac7dd17e067abea9cacd0b8e7ed102662
Roman Pavlyuk <roman.pavlyuk@gmail.com>
2023-10-24 23:44:31 +0300

    Run command enhanced

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name mbmd --hostname mbmd --tty --privileged --volume /sys/fs/cgroup:/sys/fs/cgroup -p 8080:8080 rpavlyuk/mbmd
+docker run --rm --name mbmd --hostname mbmd --tty --privileged --volume /sys/fs/cgroup:/sys/fs/cgroup -p 8080:8080 $@ rpavlyuk/mbmd
